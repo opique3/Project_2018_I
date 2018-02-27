8b2dd7556c29a10c4396427d0a16e22bc2af59d7
Genis Lleopart <genisllm@gmail.com>
2018-02-27 19:16:42 +0100

    Afegit Box-Muller

--- genis_module.f90
+++ genis_module.f90
@@ -33,22 +33,39 @@ end do
 
 end subroutine IN_positions
 
-subroutine IN_velocities(N,seed,vel)
+subroutine IN_velocities(N,T,seed,vel)
 implicit none
 INTEGER, INTENT(IN) :: N, seed
 REAL(8), DIMENSION(N,3), INTENT(IN) :: vel
 INTEGER :: i
+REAL(8) :: x1, x2, w, sigma
 
 call srand(seed)
+sigma = dsqrt(T)
 
 Do i = 1,N,1
-  vel(i,1) = rand() - 0.5
-  vel(i,2) = rand() - 0.5
-  vel(i,3) = rand() - 0.5
+  Do while(w .ge. 1.0 .or. w .eq. 0.0)
+    x1 = 2.*rand() - 1.
+    x2 = 2.*rand() - 1.
+    w = x1*x1 + x2*x2
+  end do
+  
+  w = sigma*dsqrt((-2.*log(w))/w)
+  vel(i,1) = x1*w
+  vel(i,2) = x2*w  
+
+  Do while(w .ge. 1.0 .or. w .eq. 0.0)
+    x1 = 2.*rand() - 1.
+    x2 = 2.*rand() - 1.
+    w = x1*x1 + x2*x2
+  end do
+
+  w = sigma*dsqrt((-2.*log(w))/w)
+  vel(i,3) = x1*w
+
 end do
 
 end subroutine IN_velocities
-
 
 subroutine momentum(N,vel,P_total)
 implicit none
