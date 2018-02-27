0b949872e1817dcdb136e90469360f7e3ffa5841
Oriol Piqué <piqueoriol@valhalla.qf.ub.edu>
2018-02-27 15:17:54 +0100

    Faig un split en dos moduls, amb un nom coherent amb el que fan.

---
+++ PBCmodule.f90
@@ -0,0 +1,13 @@
+module oriol
+implicit none
+contains
+
+
+! Subrutina per les condicions periòdiques de contorn per una sola partícula.
+subroutine pbc(v, boxSize)
+implicit none
+real(8), intent(in)                             :: boxSize
+real(8), dimension(3), intent(inout)            :: v
+
+v(:) = v(:) - nint(v(:)/boxSize)*boxSize
+end subroutine pbc
