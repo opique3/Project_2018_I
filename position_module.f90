f55b57008fb7a54364285c5ce1823fbd59eaa949
Genis Lleopart <genisllm@gmail.com>
2018-02-28 19:46:28 +0100

    Correccio posicions

--- position_module.f90
+++ position_module.f90
@@ -7,15 +7,12 @@ implicit none
 INTEGER, INTENT(IN) :: M, N
 REAL(8), INTENT(IN) :: density
 REAL(8), DIMENSION(N,3), INTENT(OUT) :: pos
-INTEGER :: NN, ix, iy, iz, num1, num2
+INTEGER :: ix, iy, iz, num
 REAL(8) :: a, a2, L
 REAL(8), DIMENSION(3) :: e1, e2, e3
 
 L = (N/density)**(1./3.)
 a = L/M
-a2 = a/2.
-NN = N/2
-
 e1 = (/ a, 0.0D0, 0.0D0 /)
 e2 = (/ 0.0D0, a, 0.0D0 /)
 e3 = (/ 0.0D0, 0.0D0, a /)
@@ -23,10 +20,8 @@ e3 = (/ 0.0D0, 0.0D0, a /)
 Do iz = 1,M,1
   Do iy = 1,M,1
     Do ix = 1,M,1
-        num1 = ix + (iy-1)*M + (iz-1)*M*M
-        num2 = num1 + NN
+        num = ix + (iy-1)*M + (iz-1)*M*M
         pos(num1,:) = ix*e1(:) + iy*e2(:) + iz*e3(:)
-        pos(num2,:) = pos(num1,:) + a2*e1(:) + a2*e2(:) + a2*e3(:)
     end do
   end do
 end do
