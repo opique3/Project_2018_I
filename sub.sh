531cac1f8f6555c2a47de34e5114724a38c06b25
Adria Vicens <avicensa11@gmail.com>
2018-03-01 03:41:49 +0100

    fix

--- sub.sh
+++ sub.sh
@@ -7,26 +7,25 @@
 #$ -cwd
 #$ -pe smp 1
 
-set new_dir = 'calc'
+
 setenv old `pwd`
 cd $old
-cd $calc
 
-cp ../EIA_dynamics_main.f90 .
-cp ../check_MB.f90 .
-cp ../rdf.f90 .
-cp ../Makefile .
-cp ../pbc_module.f90 .
-cp ../lj_module.f90 .
-cp ../moment_module.f90 .
-cp ../position_module.f90 .
-cp ../velocities_module.f90 .
-cp ../andersen_therm_module.f90 .
-cp ../vel_verlet_module.f90 .
-cp ../print_positions_module.f90 .
-cp ../kinetic_energy_module.f90 .
-cp ../read_data_module.f90 .
-cp ../print_data_module.f90 .
+#cp ../EIA_dynamics_main.f90 .
+#cp ../check_MB.f90 .
+#cp ../rdf.f90 .
+#cp ../Makefile .
+#cp ../pbc_module.f90 .
+#cp ../lj_module.f90 .
+#cp ../moment_module.f90 .
+#cp ../position_module.f90 .
+#cp ../velocities_module.f90 .
+#cp ../andersen_therm_module.f90 .
+#cp ../vel_verlet_module.f90 .
+#cp ../print_positions_module.f90 .
+#cp ../kinetic_energy_module.f90 .
+#cp ../read_data_module.f90 .
+#cp ../print_data_module.f90 .
 
 make compile-pbc compile-lj compile-init_pos compile-init_vel compile-thermo compile-verlet compile-KE compile-read_data compile-print_data compile-mom compile-pos compile-main compile-MB compile-rdf 
 
@@ -34,4 +33,4 @@ make compile-pbc compile-lj compile-init_pos compile-init_vel compile-thermo com
 ./check_MB velocity.out
 ./rdf traj.xyz 25
 
-rm -f *.f90 *.mod *.o dynamics check_MB rdf
+rm -f *.mod *.o dynamics check_MB rdf
