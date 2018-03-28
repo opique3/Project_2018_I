7e65269e0b73fd1f99da07046f637fbcf96eea3f
Oriol <oriol@MacBook-Pro-de-mac.local>
2018-03-28 17:07:39 +0200

    lj modified

--- Parallel/Code/lj_module.f90
+++ Parallel/Code/lj_module.f90
@@ -26,20 +26,21 @@ contains
 !Potential (V)
 
 
-subroutine LJ_pot(nPart, pos, eps, sig, boxSize, cutOff, F, V)
+subroutine LJ_pot(nPart, pos, eps, sig, boxSize, cutOff, F, V, myFirstPart, myLastPart)
 implicit none
 integer, intent(in)                             :: nPart
-real(8), dimension(nPart,3), intent(in)         :: pos
+real(8), dimension(:,:), intent(in)             :: pos
 real(8), intent(in)                             :: eps, sig, boxSize, cutOff
-real(8), dimension(nPart,3), intent(out)        :: F
+real(8), dimension(:,:), intent(out)            :: F
 real(8)                                         :: V
 real(8), dimension(3)                           :: dist
 real(8)                                         :: rij, dV
 integer                                         :: i, j, k
+integer, parameter				:: rMaster = 0
 
 V = 0.
 F(:,:) = 0.
-do i = 1, nPart, 1; do j = i + 1, nPart, 1
+do i = myFirstPart, myLastPart, 1; do j = 1, nPart, 1
         dist(:) = pos(i,:) - pos(j,:)
         call pbc(dist, boxSize)
         rij = dsqrt(dot_product(dist,dist))
@@ -48,8 +49,10 @@ do i = 1, nPart, 1; do j = i + 1, nPart, 1
                 V  = V + 4.*eps*((sig/rij)**12. - (sig/rij)**6.)
                 dV = 4*eps*(12.*sig**12./rij**13. - 6.*sig**6./rij**7)
                 F(i,:) = F(i,:) + dV*dist(:)
-                F(j,:) = F(j,:) - dV*dist(:)
         end if
 end do; end do
+
+call mpi_reduce(V, rMaster)
+
 end subroutine LJ_pot
 end module lj_module
